1839d47c824a9ea0f699aab31ec01956df8b0d1c
Gabriela Dalila Contrera <dalilagcontrera@live.com.ar>
2024-07-11 23:32:44 -0300

    se sigue con los componentes

--- alura-flix/src/components/Banner/Banner.js
+++ alura-flix/src/components/Banner/Banner.js
@@ -1,10 +1,13 @@
 import styles from "./Banner.module.css";
 
-function Banner({color}){
+function Banner({img, color}){
     return(
-
-        <div className={`${styles.gradient} ${styles['color-fondo']}`}>
+        <div className={styles.capa} style={{backgroundImage: `url("/img/banner-${img}.png")`}}>
+           
+            <div className={styles.gradient} style={{background:`${color}`}}>
           
+            </div>
+  
         </div>
 
 
--- alura-flix/src/pages/Inicio/index.js
+++ alura-flix/src/pages/Inicio/index.js
@@ -1,14 +1,17 @@
 import Header from "components/Header/Header";
 import Banner from 'components/Banner/Banner';
 import Footer from 'components/Footer/Footer';
+import Titulo from "components/Titulo/Titulo";
+
 
 function Inicio(){
     return(
         <>
            <Header></Header>
-
-           <Banner color="grey" />
-
+           <Banner img="banner" color="grey" />
+           <Titulo>
+              <h1>Los Mejores Videos Musicales</h1>
+           </Titulo> 
            <Footer></Footer>
         </>
     )
